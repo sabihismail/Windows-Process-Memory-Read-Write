46dec7afe131d23a9b1461a9e17aa82d189b42ab
Sabih Ismail <sabih.ismail@gmail.com>
2021-08-09 23:20:51 -0400

    Revert

--- src/PEProcess.cpp
+++ src/PEProcess.cpp
@@ -2,346 +2,342 @@
 
 PEProcess::PEProcess(DWORD processID)
 {
-	this->processID = processID;
+    this->processID = processID;
 
-	processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
-	processType = IdentifyProcess();
+    processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+    processType = IdentifyProcess();
 
-	GetHModules();
+    GetHModules();
 }
 
-[[maybe_unused]] PEProcess PEProcess::WaitForProcessAvailability(const char* processName, const int* toCheck)
+PEProcess PEProcess::WaitForProcessAvailability(wchar_t* processName, int* toCheck)
 {
-	ProcessType processType = IdentifyProcess();
+    ProcessType processType = IdentifyProcess();
 
-	DWORD processID = NULL;
+    DWORD processID = NULL;
 
-	while (processID == NULL && *toCheck)
-	{
-		if (processType == ProcessType::PROCESS_32)
-		{
-			processID = GetProcess32(processName);
-		}
+    while (processID == NULL && *toCheck)
+    {
+        if (processType == ProcessType::PROCESS_32)
+        {
+            processID = GetProcess32(processName);
+        }
 
-		if (processID != NULL)
-		{
-			Sleep(1000);
-		}
-	}
+        if (processID != NULL)
+        {
+            Sleep(1000);
+        }
+    }
 
-	if (!*toCheck)
-	{
-		throw std::exception("Quitting...");
-	}
+    if (!*toCheck)
+    {
+        throw std::exception("Quitting...");
+    }
 
-	return PEProcess(processID);
+    return PEProcess(processID);
 }
 
-[[maybe_unused]] int PEProcess::StillAlive()
+int PEProcess::StillAlive()
 {
-	DWORD ret = WaitForSingleObject(processHandle, 0);
+    DWORD ret = WaitForSingleObject(processHandle, 0);
 
-	return ret == WAIT_TIMEOUT;
+    return ret == WAIT_TIMEOUT;
 }
 
-[[maybe_unused]] void PEProcess::SetModule(const char* moduleName)
+void PEProcess::SetModule(wchar_t* moduleName)
 {
-	std::string name(moduleName);
-	std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+    std::wstring name(moduleName);
+    std::transform(name.begin(), name.end(), name.begin(), ::tolower);
 
-	if (processType == ProcessType::PROCESS_32)
-	{
-		if (hModules32.find(name) == hModules32.end())
-		{
-			throw std::exception("Module not found.");
-		}
+    if (processType == ProcessType::PROCESS_32)
+    {
+        if (hModules32.find(name) == hModules32.end())
+        {
+            throw std::exception("Module not found.");
+        }
 
-		currentModuleName = name;
-		currentHModule = &hModules32[currentModuleName];
-	}
+        currentModuleName = &name;
+        currentHModule = &hModules32[name];
+    }
 }
 
-[[maybe_unused]] void PEProcess::SetEndianness(EndianType endian)
+void PEProcess::SetEndianness(EndianType endian)
 {
-	endianType = endian;
+    endianType = endian;
 }
 
-[[maybe_unused]] void PEProcess::CheckModule(const std::string& section)
+void PEProcess::CheckModule(std::string section)
 {
-	if (currentHModule == nullptr || currentModuleName.empty())
-	{
-		throw std::exception("No module set.");
-	}
+    if (currentHModule == nullptr || currentModuleName == nullptr || currentModuleName->empty())
+    {
+        throw std::exception("No module set.");
+    }
 
-	if (currentHModule->sections.find(section) == currentHModule->sections.end())
-	{
-		throw std::exception("Section not found.");
-	}
+    if (currentHModule->sections.find(section) == currentHModule->sections.end())
+    {
+        throw std::exception("Section not found.");
+    }
 }
 
 LPVOID PEProcess::CheckAddress(uintptr_t offset, bool directAddress)
 {
-	if (directAddress)
-	{
-		return (LPVOID)offset;
-	}
+    if (directAddress)
+    {
+        return (LPVOID)offset;
+    }
 
-	auto moduleBaseAddress = (uintptr_t)currentHModule->hModule.modBaseAddr;
-	uintptr_t address = moduleBaseAddress + offset;
+    uintptr_t moduleBaseAddress = (uintptr_t)currentHModule->hModule.modBaseAddr;
+    uintptr_t address = moduleBaseAddress + offset;
 
-	uintptr_t maxModuleAddress = moduleBaseAddress + currentHModule->hModule.modBaseSize;
+    uintptr_t maxModuleAddress = moduleBaseAddress + currentHModule->hModule.modBaseSize;
 
-	// basic insecure check, probably need to fix
-	if (address < moduleBaseAddress || address >= maxModuleAddress)
-	{
-		throw std::exception("Address is out of current module bounds.");
-	}
+    // basic insecure check, probably need to fix
+    if (address < moduleBaseAddress || address >= maxModuleAddress)
+    {
+        throw std::exception("Address is out of current module bounds.");
+    }
 
-	return (LPVOID)address;
+    return (LPVOID)address;
 }
 
-[[maybe_unused]] LPVOID PEProcess::ReadMemoryAddressChain(uintptr_t firstAddress, int* offsets, uint16_t offsetCount, EndianType endianFlip)
+LPVOID PEProcess::ReadMemoryAddressChain(uintptr_t firstAddress, int* offsets, uint16_t offsetCount, EndianType endianFlip)
 {
-	uintptr_t currentAddress = firstAddress;
-	for (uint16_t i = 0; i <= offsetCount; i++)
-	{
-		LPVOID address = ReadMemoryAddress(currentAddress, currentAddress != firstAddress, endianFlip);
-		currentAddress = (uintptr_t)address;
+    uintptr_t currentAddress = firstAddress;
+    for (uint16_t i = 0; i <= offsetCount; i++)
+    {
+        LPVOID address = ReadMemoryAddress(currentAddress, currentAddress == firstAddress ? 0 : 1, endianFlip);
+        currentAddress = (uintptr_t)address;
 
-		if (i != offsetCount)
-		{
-			currentAddress += *offsets++;
-		}
-	}
+        if (i != offsetCount)
+        {
+            currentAddress += *offsets++;
+        }
+    }
 
-	return (LPVOID)currentAddress;
+    return (LPVOID)currentAddress;
 }
 
-[[maybe_unused]] int PEProcess::ReadMemoryStruct(LPVOID address, void* obj, SIZE_T size, int offset)
+int PEProcess::ReadMemoryStruct(LPVOID address, void* obj, SIZE_T size, int offset)
 {
-	uintptr_t ptr = (uintptr_t)address + offset;
-	auto addr = (LPCVOID)ptr;
+    uintptr_t ptr = (uintptr_t)address + offset;
+    LPCVOID addr = (LPCVOID)ptr;
 
-	return ReadProcessMemory(processHandle, addr, obj, size, nullptr);
+    return ReadProcessMemory(processHandle, addr, obj, size, NULL);
 }
 
-[[maybe_unused]] std::string PEProcess::ReadMemoryStringFromAddress(uintptr_t offset, int length, int* result, bool directAddress, EndianType endianness)
+std::string PEProcess::ReadMemoryStringFromAddress(uintptr_t offset, int length, int* result, bool directAddress, EndianType endianness)
 {
-	LPVOID address = ReadMemoryAddress(offset, false, endianness);
-	auto addressConv = (uintptr_t)address;
-	std::string str = ReadMemoryString(addressConv, length, result, directAddress);
+    LPVOID address = ReadMemoryAddress(offset, 0, endianness);
+    uintptr_t addressConv = (uintptr_t)address;
+    std::string str = ReadMemoryString(addressConv, length, result, directAddress);
 
-	return str;
+    return str;
 }
 
-[[maybe_unused]] LPVOID PEProcess::ReadMemoryAddress(LPVOID address, EndianType endianness)
+LPVOID PEProcess::ReadMemoryAddress(LPVOID address, EndianType endianness)
 {
-	return ReadMemoryAddress((uintptr_t)address, true, endianness);
+    return ReadMemoryAddress((uintptr_t)address, 1, endianness);
 }
 
 LPVOID PEProcess::ReadMemoryAddress(uintptr_t offset, bool directAddress, EndianType endianness)
 {
-	LPVOID address = CheckAddress(offset, directAddress);
+    LPVOID address = CheckAddress(offset, directAddress);
 
-	if (processType == ProcessType::PROCESS_32)
-	{
-		LPVOID buffer{};
-		bool result = ReadProcessMemory(processHandle, address, &buffer, sizeof(buffer), nullptr);
+    if (processType == ProcessType::PROCESS_32)
+    {
+        LPVOID buffer{};
+        bool result = ReadProcessMemory(processHandle, address, &buffer, sizeof(buffer), NULL);
 
-		if (!result)
-		{
-			throw std::exception("ReadMemoryAddress Failed.");
-		}
+        if (endianness == EndianType::BIG_ENDIAN || endianType == EndianType::BIG_ENDIAN)
+        {
+            unsigned long val = (unsigned long)buffer;
 
-		if (endianness == EndianType::BIG_ENDIAN || endianType == EndianType::BIG_ENDIAN)
-		{
-			auto val = (unsigned long long)buffer;
+            val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF);
+            val = (val << 16) | (val >> 16);
 
-			val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF);
-			val = (val << 16) | (val >> 16);
+            buffer = (LPVOID)val;
+        }
 
-			buffer = (LPVOID)val;
-		}
+        return buffer;
+    }
+    else if (processType == ProcessType::PROCESS_64)
+    {
+        
+    }
 
-		return buffer;
-	}
-	else if (processType == ProcessType::PROCESS_64)
-	{
-
-	}
-
-	throw std::exception("Invalid architecture type.");
+    throw std::exception("Invalid architecture type.");
 }
 
-[[maybe_unused]] std::string PEProcess::ReadMemoryString(LPVOID address, int length, int* result, int offset)
+std::string PEProcess::ReadMemoryString(LPVOID address, int length, int* result, int offset)
 {
-	if (address == nullptr)
-	{
-		return "";
-	}
+    if (address == 0)
+    {
+        return "";
+    }
 
-	uintptr_t addr = (uintptr_t)address + offset;
+    uintptr_t addr = (uintptr_t)address + offset;
 
-	return ReadMemoryString(addr, length, result, true);
+    return ReadMemoryString(addr, length, result, 1);
 }
 
 std::string PEProcess::ReadMemoryString(uintptr_t offset, int length, int* result, bool directAddress)
 {
-	LPVOID address = CheckAddress(offset, directAddress);
+    LPVOID address = CheckAddress(offset, directAddress);
 
-	SIZE_T totalRead = 0;
-	std::string str;
-	do
-	{
-		char buffer[256]{};
+    SIZE_T totalRead = 0;
+    std::string str;
+    do
+    {
+        char buffer[256]{};
 
-		SIZE_T read = 0;
-		int curResult = ReadProcessMemory(processHandle, address, &buffer, sizeof(buffer), &read);
-		totalRead += read;
+        SIZE_T read = 0;
+        int curResult = ReadProcessMemory(processHandle, address, &buffer, sizeof(buffer), &read);
+        totalRead += read;
 
-		if (curResult)
-		{
-			for (auto c : buffer)
-			{
-				if (c == '\0')
-				{
-					return str;
-				}
+        if (curResult)
+        {
+            for (auto c : buffer)
+            {
+                if (c == '\0')
+                {
+                    return str;
+                }
 
-				str += c;
-			}
+                str += c;
+            }
 
-			address = (LPVOID)((unsigned long long)address + read);
-		}
-		else
-		{
-			if (result)
-			{
-				*result = 0;
-			}
+            address = (LPVOID)((unsigned long)address + read);
+        }
+        else
+        {
+            if (result)
+            {
+                *result = 0;
+            }
 
-			return "";
-		}
-	} while (totalRead < length);
+            return "";
+        }
+    } while (totalRead < length);
 
-	if (result)
-	{
-		*result = 1;
-	}
+    if (result)
+    {
+        *result = 1;
+    }
 
-	return str;
+    return str;
 }
 
-DWORD PEProcess::GetProcess32(const char* processName)
+DWORD PEProcess::GetProcess32(wchar_t* processName)
 {
-	PROCESSENTRY32 entry{};
-	entry.dwSize = sizeof(PROCESSENTRY32);
+    PROCESSENTRY32 entry{};
+    entry.dwSize = sizeof(PROCESSENTRY32);
 
-	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
 
-	if (Process32First(snapshot, &entry) == TRUE)
-	{
-		while (Process32Next(snapshot, &entry) == TRUE)
-		{
-			if (strcmp(entry.szExeFile, processName) == 0)
-			{
-				return entry.th32ProcessID;
-			}
-		}
-	}
+    HANDLE hProcess = nullptr;
+    if (Process32First(snapshot, &entry) == TRUE)
+    {
+        while (Process32Next(snapshot, &entry) == TRUE)
+        {
+            if (wcscmp(entry.szExeFile, processName) == 0)
+            {
+                return entry.th32ProcessID;
+            }
+        }
+    }
 
-	return NULL;
+    return NULL;
 }
 
 ProcessType PEProcess::IdentifyProcess()
 {
 #if _WIN32
-	return ProcessType::PROCESS_32;
+    return ProcessType::PROCESS_32;
 #endif
 
 #if _WIN64 // Not implemented so just exit
-	exit(-1);
-	return ProcessType::PROCESS_64;
+    exit(-1);
+    return ProcessType::PROCESS_64;
 #endif
 }
 
 void PEProcess::GetHModules()
 {
-	switch (processType)
-	{
-		case ProcessType::PROCESS_32:
-			GetHModules32();
-			break;
-		case ProcessType::PROCESS_64:
-			break;
-	}
+    switch (processType)
+    {
+    case ProcessType::PROCESS_32:
+        GetHModules32();
+        break;
+    case ProcessType::PROCESS_64:
+        break;
+    }
 }
 
 void PEProcess::GetHModules32()
 {
-	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, processID);
-	if (hSnapshot != INVALID_HANDLE_VALUE)
-	{
-		MODULEENTRY32 moduleEntry32{};
-		moduleEntry32.dwSize = sizeof(MODULEENTRY32);
-		if (Module32First(hSnapshot, &moduleEntry32))
-		{
-			do
-			{
-				ProcessHModule32(moduleEntry32);
-			} while (Module32Next(hSnapshot, &moduleEntry32));
-		}
-		CloseHandle(hSnapshot);
-	}
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, processID);
+    if (hSnapshot != INVALID_HANDLE_VALUE)
+    {
+        MODULEENTRY32 moduleEntry32{};
+        moduleEntry32.dwSize = sizeof(MODULEENTRY32);
+        if (Module32First(hSnapshot, &moduleEntry32))
+        {
+            do
+            {
+                ProcessHModule32(moduleEntry32);
+            } while (Module32Next(hSnapshot, &moduleEntry32));
+        }
+        CloseHandle(hSnapshot);
+    }
 }
 
-[[maybe_unused]] void PEProcess::ProcessHModule32(MODULEENTRY32 hModule)
+void PEProcess::ProcessHModule32(MODULEENTRY32W hModule)
 {
-	auto addr = (unsigned long long)hModule.modBaseAddr;
+    unsigned long addr = (unsigned long)hModule.modBaseAddr;
 
-	IMAGE_DOS_HEADER dosHeaders{};
-	ReadProcessMemory(processHandle, (LPCVOID)addr, &dosHeaders, sizeof(IMAGE_DOS_HEADER), nullptr);
-	if (dosHeaders.e_magic != IMAGE_DOS_SIGNATURE)
-	{
-		return;
-	}
+    IMAGE_DOS_HEADER dosHeaders{};
+    ReadProcessMemory(processHandle, (LPCVOID)addr, &dosHeaders, sizeof(IMAGE_DOS_HEADER), 0);
+    if (dosHeaders.e_magic != IMAGE_DOS_SIGNATURE)
+    {
+        return;
+    }
 
-	addr += dosHeaders.e_lfanew;
+    addr += dosHeaders.e_lfanew;
 
-	IMAGE_NT_HEADERS ntHeaders{};
-	ReadProcessMemory(processHandle, (LPCVOID)addr, &ntHeaders, sizeof(IMAGE_NT_HEADERS), nullptr);
+    IMAGE_NT_HEADERS ntHeaders{};
+    ReadProcessMemory(processHandle, (LPCVOID)addr, &ntHeaders, sizeof(IMAGE_NT_HEADERS), 0);
 
-	if (ntHeaders.OptionalHeader.Magic != IMAGE_NT_OPTIONAL_HDR_MAGIC)
-	{
-		return;
-	}
+    if (ntHeaders.OptionalHeader.Magic != IMAGE_NT_OPTIONAL_HDR_MAGIC)
+    {
+        return;
+    }
 
-	addr += sizeof(ntHeaders);
+    addr += sizeof(ntHeaders);
 
-	std::map<std::string, IMAGE_SECTION_HEADER> sections;
-	for (WORD i = 0; i < ntHeaders.FileHeader.NumberOfSections; i++)
-	{
-		IMAGE_SECTION_HEADER section{};
-		ReadProcessMemory(processHandle, (LPCVOID)addr, &section, sizeof(IMAGE_SECTION_HEADER), nullptr);
+    std::map<std::string, IMAGE_SECTION_HEADER> sections;
+    for (WORD i = 0; i < ntHeaders.FileHeader.NumberOfSections; i++)
+    {
+        IMAGE_SECTION_HEADER section{};
+        ReadProcessMemory(processHandle, (LPCVOID)addr, &section, sizeof(IMAGE_SECTION_HEADER), 0);
 
-		std::string name(reinterpret_cast<char*>(section.Name));
+        std::string name(reinterpret_cast<char*>(section.Name));
 
-		sections[name] = section;
+        sections[name] = section;
 
-		addr += sizeof(IMAGE_SECTION_HEADER);
-	}
+        addr += sizeof(IMAGE_SECTION_HEADER);
+    }
 
-	std::string name(hModule.szModule);
-	std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+    std::wstring name(hModule.szModule);
+    std::transform(name.begin(), name.end(), name.begin(), ::tolower);
 
-	HModuleExt32 obj
-	{
-		hModule = hModule,
-		sections = sections,
-		dosHeaders = dosHeaders,
-		ntHeaders = ntHeaders,
-		name = name,
-	};
+    HModuleExt32 obj
+    {
+        dosHeaders = dosHeaders,
+        ntHeaders = ntHeaders,
+        hModule = hModule,
+        name = name,
+        sections = sections
+    };
 
-	hModules32[name] = obj;
+    hModules32[name] = obj;
 }
--- src/PEProcess.h
+++ src/PEProcess.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <Windows.h>
+#include <windows.h>
 #include <tchar.h>
 #include <cstdio>
-#include <TlHelp32.h>
-#include <DbgHelp.h>
+#include <tlhelp32.h>
+#include <dbghelp.h>
 #include <string>
 #include <vector>
 #include <map>
@@ -13,12 +13,11 @@
 
 struct HModuleExt32
 {
-	MODULEENTRY32 hModule;
+	IMAGE_DOS_HEADER dosHeaders;
+	IMAGE_NT_HEADERS ntHeaders;
+	MODULEENTRY32W hModule;
+	std::wstring name;
 	std::map<std::string, IMAGE_SECTION_HEADER> sections;
-
-	[[maybe_unused]] IMAGE_DOS_HEADER dosHeaders;
-	[[maybe_unused]] IMAGE_NT_HEADERS ntHeaders;
-	[[maybe_unused]] std::string name;
 };
 
 enum class ProcessType
@@ -33,53 +32,51 @@ enum class EndianType
 	BIG_ENDIAN
 };
 
-class [[maybe_unused]] PEProcess
+class PEProcess
 {
 public:
-	explicit PEProcess(DWORD processID);
+	PEProcess(DWORD processID);
+	static PEProcess WaitForProcessAvailability(wchar_t* processName, int* toCheck);
+	int StillAlive();
+	void SetModule(wchar_t* moduleName);
+	void SetEndianness(EndianType endian);
+	LPVOID ReadMemoryAddressChain(uintptr_t firstAddress, int* offsets, uint16_t offsetCount, EndianType endianFlip = EndianType::LITTLE_ENDIAN);
+	std::string ReadMemoryStringFromAddress(uintptr_t offset, int length = 32, int* result = nullptr, bool directAddress = false, EndianType endianFlip = EndianType::LITTLE_ENDIAN);
+	LPVOID ReadMemoryAddress(LPVOID address, EndianType endian = EndianType::LITTLE_ENDIAN);
+	LPVOID ReadMemoryAddress(uintptr_t offset, bool directAddress = false, EndianType endian = EndianType::LITTLE_ENDIAN);
+	std::string ReadMemoryString(LPVOID address, int length = 32, int* result = nullptr, int offset = 0);
 	std::string ReadMemoryString(uintptr_t offset, int length = 32, int* result = nullptr, bool directAddress = false);
-	[[maybe_unused]] LPVOID ReadMemoryAddress(uintptr_t offset, bool directAddress = false, EndianType endian = EndianType::LITTLE_ENDIAN);
-
-	[[maybe_unused]] static PEProcess WaitForProcessAvailability(const char* processName, const int* toCheck);
-	[[maybe_unused]] int StillAlive();
-	[[maybe_unused]] void SetModule(const char* moduleName);
-	[[maybe_unused]] void SetEndianness(EndianType endian);
-	[[maybe_unused]] LPVOID ReadMemoryAddressChain(uintptr_t firstAddress, int* offsets, uint16_t offsetCount, EndianType endianFlip = EndianType::LITTLE_ENDIAN);
-	[[maybe_unused]] std::string ReadMemoryStringFromAddress(uintptr_t offset, int length = 32, int* result = nullptr, bool directAddress = false, EndianType endianFlip = EndianType::LITTLE_ENDIAN);
-	[[maybe_unused]] LPVOID ReadMemoryAddress(LPVOID address, EndianType endian = EndianType::LITTLE_ENDIAN);
-	[[maybe_unused]] std::string ReadMemoryString(LPVOID address, int length = 32, int* result = nullptr, int offset = 0);
-	[[maybe_unused]] int ReadMemoryStruct(LPVOID address, void* obj, SIZE_T size, int offset = 0);
-
+	int ReadMemoryStruct(LPVOID address, void* obj, SIZE_T size, int offset = 0);
 	template<class T>
-	[[maybe_unused]] T ReadMemoryStruct(LPVOID address, int offset = 0, int* success = nullptr);
+	T ReadMemoryStruct(LPVOID address, int offset = 0, int* success = nullptr);
 
 private:
-	HANDLE processHandle;
+	wchar_t* processName;
 	DWORD processID;
+	HANDLE processHandle;
 	ProcessType processType;
 	EndianType endianType = EndianType::LITTLE_ENDIAN;
-	std::map<std::string, HModuleExt32> hModules32;
-	std::string currentModuleName{};
+	std::map<std::wstring, HModuleExt32> hModules32;
+
+	std::wstring* currentModuleName;
 	HModuleExt32* currentHModule = nullptr;
-	[[maybe_unused]] wchar_t* processName{};
 
 	static ProcessType IdentifyProcess();
-	static DWORD GetProcess32(const char* processName);
-
+	static DWORD GetProcess32(wchar_t* processName);
+	void CheckModule(std::string section);
 	LPVOID CheckAddress(uintptr_t address, bool directAddress);
 	void GetHModules();
 	void GetHModules32();
-	void ProcessHModule32(MODULEENTRY32 hModule);
-	[[maybe_unused]] void CheckModule(const std::string& section);
+	void ProcessHModule32(MODULEENTRY32W hModule);
 };
 
 template<class T>
-[[maybe_unused]] T PEProcess::ReadMemoryStruct(LPVOID address, int offset, int* success)
+T PEProcess::ReadMemoryStruct(LPVOID address, int offset, int* success)
 {
 	T obj{};
 
 	uintptr_t ptr = (uintptr_t)address + offset;
-	auto addr = (LPCVOID)ptr;
+	LPCVOID addr = (LPCVOID)ptr;
 
 	int result = ReadProcessMemory(processHandle, addr, &obj, sizeof(T), nullptr);
 
